aa891ee0cbfad8285ba84340b7f8575beb5efd8f
Miguel del Mazo <migueldelmazo@gmail.com>
2017-08-15 14:46:37 +0200

    Added lodash mixins

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const _ = require('lodash'),
+const _ = require('./lodash'),
   bodyParser = require('body-parser'),
   express = require('express'),
   path = require('path'),
---
+++ server/lodash.js
@@ -0,0 +1,38 @@
+const _ = require('lodash')
+
+_.mixin({
+
+  parseArray: function (arr) {
+    arr = arr || []
+    return _.isArray(arr) ? arr : [arr]
+  },
+
+  stack: function (removeLevels = 1) {
+    const stack = new Error().stack,
+      stackArr = stack.split('\n')
+    removeLevels = removeLevels || 1
+    stackArr.splice(1, removeLevels)
+    return stackArr.join('')
+  },
+
+  stringify: function (obj) {
+    const cache = []
+    return JSON.stringify(obj, function (key, value) {
+      if (_.isString(value) || _.isNumber(value) || _.isBoolean(value)) {
+        return value
+      } else if (_.isError(value)) {
+        return _.get(value, 'stack', '').replace(/\\n/g, '')
+      } else if (_.isPlainObject(value) || _.isArray(value)) {
+        if (cache.indexOf(value) !== -1) {
+          return
+        } else {
+          cache.push(value)
+          return value
+        }
+      }
+    })
+  }
+
+})
+
+module.exports = _
